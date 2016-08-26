32a1a93d5e807389b83e5ad5b525ed2fd9dac4f8
Kev Simpson <hello@kev-simpson.me>
2016-08-26 17:02:25 +0100

    Refactor is modern test to avoid potential JS errors in old browsers

--- _assets/js/utils/is-modern.js
+++ _assets/js/utils/is-modern.js
@@ -1,16 +1,17 @@
 'use strict';
 var IsModern = (function () {
-	var $window = window,
-		$html = document.querySelector('html');
-
+	var $window = window;
 
 	var isModern = function() {
 		return ( 'querySelector' in document && 'localStorage' in $window && 'addEventListener' in $window );
 	};
 
 	var _enhanceStyles = function() {
+		var $html;
 
 		if( isModern ){
+			$html = document.querySelector('html');
+
 			if( Modernizr.classlist && $html.classList.contains('no-enhance') ) {
 				$html.classList.remove('no-enhance');
 				$html.classList.add('enhance');
@@ -23,7 +24,6 @@ var IsModern = (function () {
 	if( isModern() ) {
 		_enhanceStyles();
 	}
-
 
   return {
     isModern: isModern
